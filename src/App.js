47c3c5a11ba063408bce9aae53d40c8a7dfffa61
Nabin Chakraborty <83083709+nabinchakraborty@users.noreply.github.com>
2023-02-15 23:45:25 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import Login from "./Components/Login/Login";
+import SignUp from "./Components/Signup/SignUp";
+import { auth } from "./firebase";
+import Todo from "./Todo/Todo";
+
+export default function App() {
+  const [username, setUsername] = useState("");
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUsername(user.email.substr(0, user.email.indexOf("@")).toUpperCase());
+      } else setUsername("");
+    });
+  }, []);
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/todo" element={<Todo name={username}/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+
+
+
+
+
